a68f4971867362e0aa6a8e65516a8782fc2c2c6d
enbraining <me@xolving.com>
2024-05-05 14:23:24 +0900

    소켓 핸들러에서 함수 분리

--- src/main/kotlin/team/underlive/underlive/global/socket/handler/WebSocketHandler.kt
+++ src/main/kotlin/team/underlive/underlive/global/socket/handler/WebSocketHandler.kt
@@ -8,81 +8,25 @@ import org.springframework.web.socket.TextMessage
 import org.springframework.web.socket.WebSocketSession
 import org.springframework.web.socket.handler.TextWebSocketHandler
 import team.underlive.underlive.global.socket.dto.ChatMessage
-import team.underlive.underlive.domain.room.entity.RoomEntity
-import team.underlive.underlive.domain.room.repository.RoomRepository
-import team.underlive.underlive.domain.room.service.RoomService
-import team.underlive.underlive.domain.session.entity.SessionEntity
-import team.underlive.underlive.domain.session.repository.SessionRepository
 import team.underlive.underlive.global.socket.service.SocketService
-import java.util.*
 
 @Component
-@Transactional
 class WebSocketHandler(
 	private val objectMapper: ObjectMapper,
-	private val roomService: RoomService,
 	private val socketService: SocketService,
-	private val sessionRepository: SessionRepository,
-	private val roomRepository: RoomRepository,
 ) : TextWebSocketHandler() {
 	override fun handleTextMessage(session: WebSocketSession, message: TextMessage) {
 		val chatMessage: ChatMessage = objectMapper.readValue(message.payload, ChatMessage::class.java)
-		socketService.handlerActions(session, chatMessage, roomService)
+		socketService.handlerActions(session, chatMessage)
 	}
 
 	override fun afterConnectionEstablished(session: WebSocketSession) {
-		session.sendMessage(TextMessage("{\"status\":\"WAIT\"}"))
-
-		val sessionEntity = SessionEntity(null, UUID.fromString(session.id))
-		sessionRepository.save(sessionEntity)
-
-		socketService.sessions[UUID.fromString(session.id)] = session
-
-		val rooms = roomRepository.findRoomsWithSingleSession()
-
-		if(rooms.isEmpty()){
-			val roomEntity = RoomEntity(null, mutableListOf(sessionEntity))
-			roomRepository.save(roomEntity)
-			rooms.add(roomEntity)
-		} else {
-			val roomEntity = rooms[0]
-			roomEntity.sessions.add(sessionEntity)
-			roomRepository.save(roomEntity)
-			rooms.add(roomEntity)
-		}
-
-		if(rooms[0].sessions.size == 2){
-			socketService.sessions.map { (key, value) -> run {
-				val currentSession = sessionRepository.findBySocket(key)
-				if(rooms[0].sessions.isNotEmpty() && rooms[0].sessions.contains(currentSession.get())){
-					value.sendMessage(TextMessage("{\"status\":\"JOIN\"}"))
-				}
-			}}
-		}
-
+		socketService.estabilshedConnection(session)
 		super.afterConnectionEstablished(session)
 	}
 
 	override fun afterConnectionClosed(session: WebSocketSession, status: CloseStatus) {
-		socketService.sessions.remove(UUID.fromString(session.id))
-
-		val sessionEntity = sessionRepository.findBySocket(UUID.fromString(session.id))
-
-		if(sessionEntity.isPresent){
-			val roomEntity = roomRepository.findBySessionsContains(sessionEntity.get())
-
-			socketService.sessions.map { (key, value) -> run {
-				val currentSession = sessionRepository.findBySocket(key)
-				if(roomEntity.get().sessions.contains(currentSession.get())){
-					value.sendMessage(TextMessage("{\"status\":\"EXIT\"}"))
-					value.close()
-					sessionRepository.delete(currentSession.get())
-				}
-			}}
-
-			roomRepository.deleteBySessionsContains(sessionEntity.get())
-		}
-
+		socketService.closedConnection(session)
 		super.afterConnectionClosed(session, status)
 	}
 }
--- src/main/kotlin/team/underlive/underlive/global/socket/service/SocketService.kt
+++ src/main/kotlin/team/underlive/underlive/global/socket/service/SocketService.kt
@@ -5,8 +5,9 @@ import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import org.springframework.web.socket.TextMessage
 import org.springframework.web.socket.WebSocketSession
+import team.underlive.underlive.domain.room.entity.RoomEntity
 import team.underlive.underlive.domain.room.repository.RoomRepository
-import team.underlive.underlive.domain.room.service.RoomService
+import team.underlive.underlive.domain.session.entity.SessionEntity
 import team.underlive.underlive.domain.session.repository.SessionRepository
 import team.underlive.underlive.global.socket.dto.ChatMessage
 import java.util.*
@@ -16,7 +17,7 @@ import java.util.concurrent.ConcurrentHashMap
 class SocketService(
 	private val roomRepository: RoomRepository,
 	private val sessionRepository: SessionRepository,
-	private val objectMapper: ObjectMapper,
+	private val objectMapper: ObjectMapper
 ){
 	val sessions = ConcurrentHashMap<UUID, WebSocketSession>()
 
@@ -25,7 +26,7 @@ class SocketService(
 	}
 
 	@Transactional
-	fun handlerActions(session: WebSocketSession, chatMessage: ChatMessage, roomService: RoomService) {
+	fun handlerActions(session: WebSocketSession, chatMessage: ChatMessage) {
 		val sessionEntity = sessionRepository.findBySocket(UUID.fromString(session.id))
 
 		if (sessionEntity.isPresent) {
@@ -45,6 +46,60 @@ class SocketService(
 			} else {
 				sessionRepository.delete(sessionEntity.get())
 			}
+		}
+	}
+
+	@Transactional
+	fun estabilshedConnection(session: WebSocketSession){
+		session.sendMessage(TextMessage("{\"status\":\"WAIT\"}"))
+
+		val sessionEntity = SessionEntity(null, UUID.fromString(session.id))
+		sessionRepository.save(sessionEntity)
+
+		sessions[UUID.fromString(session.id)] = session
+
+		val rooms = roomRepository.findRoomsWithSingleSession()
+
+		if(rooms.isEmpty()){
+			val roomEntity = RoomEntity(null, mutableListOf(sessionEntity))
+			roomRepository.save(roomEntity)
+			rooms.add(roomEntity)
+		} else {
+			val roomEntity = rooms[0]
+			roomEntity.sessions.add(sessionEntity)
+			roomRepository.save(roomEntity)
+			rooms.add(roomEntity)
+		}
+
+		if(rooms[0].sessions.size == 2){
+			sessions.map { (key, value) -> run {
+				val currentSession = sessionRepository.findBySocket(key)
+				if(rooms[0].sessions.isNotEmpty() && rooms[0].sessions.contains(currentSession.get())){
+					value.sendMessage(TextMessage("{\"status\":\"JOIN\"}"))
+				}
+			}}
+		}
+	}
+
+	@Transactional
+	fun closedConnection(session: WebSocketSession){
+		sessions.remove(UUID.fromString(session.id))
+
+		val sessionEntity = sessionRepository.findBySocket(UUID.fromString(session.id))
+
+		if(sessionEntity.isPresent){
+			val roomEntity = roomRepository.findBySessionsContains(sessionEntity.get())
+
+			sessions.map { (key, value) -> run {
+				val currentSession = sessionRepository.findBySocket(key)
+				if(roomEntity.get().sessions.contains(currentSession.get())){
+					value.sendMessage(TextMessage("{\"status\":\"EXIT\"}"))
+					value.close()
+					sessionRepository.delete(currentSession.get())
+				}
+			}}
+
+			roomRepository.deleteBySessionsContains(sessionEntity.get())
 		}
 	}
 }
