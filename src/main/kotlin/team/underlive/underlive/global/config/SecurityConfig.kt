4ac722cfea2ac0398caf989b947f5104e767ce1f
enbraining <me@xolving.com>
2024-06-16 23:27:09 +0900

    :recycle: cors

--- src/main/kotlin/team/underlive/underlive/global/config/SecurityConfig.kt
+++ src/main/kotlin/team/underlive/underlive/global/config/SecurityConfig.kt
@@ -44,7 +44,8 @@ class SecurityConfig {
 
 		config.allowCredentials = true
 		config.allowedOrigins = mutableListOf("*")
-		config.allowedMethods = mutableListOf("*")
+		config.allowedMethods =
+			mutableListOf("GET", "POST", "PUT", "DELETE", "PATCH", "HEAD", "OPTIONS")
 		config.allowedHeaders = mutableListOf("*")
 		config.exposedHeaders = mutableListOf("*")
 		config.maxAge = 86400L
